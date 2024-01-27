78c2eeb1df0e23d3144d5fe6b5d9d6bb5778255b
zulhakki <zulhakki60@gmail.com>
2024-01-27 12:21:40 +0800

    membangun_website

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost","root","","kampus");
+// if ($conn == true){
+//     echo "berhasil terhubung kedatabase";
+// } else {
+//     echo "gagal terhubung kedatabase";
+// }
